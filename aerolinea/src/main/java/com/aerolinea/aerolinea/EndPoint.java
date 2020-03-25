a9a9c6af9021b59d575181243cb075ff70b6af71
yaelmo <yaelmo@localhost>
2020-03-25 17:55:57 -0600

    Primera versión aerolínea

--- aerolinea/src/main/java/com/aerolinea/aerolinea/EndPoint.java
+++ aerolinea/src/main/java/com/aerolinea/aerolinea/EndPoint.java
@@ -1,5 +1,11 @@
 package com.aerolinea.aerolinea;
 
+import org.example.aerolinea.AsientosRequest;
+import org.example.aerolinea.AsientosResponse;
+import org.example.aerolinea.CancelCompraRequest;
+import org.example.aerolinea.CancelCompraResponse;
+import org.example.aerolinea.CompraRequest;
+import org.example.aerolinea.CompraResponse;
 import org.example.aerolinea.VuelosRequest;
 import org.example.aerolinea.VuelosResponse;
 import org.springframework.ws.server.endpoint.annotation.Endpoint;
@@ -9,6 +15,7 @@ import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
 
 @Endpoint
 public class EndPoint {
+	
 	@PayloadRoot(namespace="http://www.example.org/aerolinea", localPart="VuelosRequest")
 	@ResponsePayload
 	
@@ -25,4 +32,46 @@ public class EndPoint {
 		return respuesta;
 		
 	}
+	
+	
+	@PayloadRoot(namespace="http://www.example.org/aerolinea", localPart="AsientosRequest")
+	@ResponsePayload
+	public AsientosResponse getAsientos(@RequestPayload AsientosRequest peticion) {
+		
+		AsientosResponse respuesta = new AsientosResponse();
+		respuesta.setIdAsiento("A19");
+		respuesta.setAsientosDisponibles(1);
+		
+		return respuesta;
+		
+	}
+	
+	
+	@PayloadRoot(namespace="http://www.example.org/aerolinea", localPart="CompraRequest")
+	@ResponsePayload
+	public CompraResponse getCompra(@RequestPayload CompraRequest peticion) {
+		CompraResponse respuesta = new CompraResponse();
+			respuesta.setStatus(true);
+			respuesta.setOrigin("México City");
+			respuesta.setDestination("Monterrey Nuevo León");
+			respuesta.setDate("2020-03-25");
+			respuesta.setNoTicket(37478);
+		
+		return respuesta;
+	}
+
+
+	@PayloadRoot(namespace="http://www.example.org/aerolinea", localPart="CancelCompraRequest")
+	@ResponsePayload
+	
+	public CancelCompraResponse getCancel(@RequestPayload  CancelCompraRequest peticion) {
+		
+		CancelCompraResponse respuesta = new CancelCompraResponse();
+		respuesta.setStatus(true);
+		respuesta.setMessage("Su compra ha sido cancelada");
+		return respuesta;
+	}
+	
+	
+
 }
