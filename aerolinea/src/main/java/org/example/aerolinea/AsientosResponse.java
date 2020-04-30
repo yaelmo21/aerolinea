65441cbdb6220fb4ce3f4b61c2cd9fb6d2728773
yaelmo <yaelmo@localhost>
2020-04-30 13:20:05 -0500

    Update

--- aerolinea/src/main/java/org/example/aerolinea/AsientosResponse.java
+++ aerolinea/src/main/java/org/example/aerolinea/AsientosResponse.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2020.04.04 a las 05:01:17 PM CST 
+// Generado el: 2020.04.10 a las 01:59:06 AM CDT 
 //
 
 
@@ -152,7 +152,7 @@ public class AsientosResponse {
 	public void setAlert(List<String> alert) {
 		this.alert = alert;
 	}
-    
-    
 
+    
+    
 }
--- aerolinea/src/main/java/org/example/aerolinea/package-info.java
+++ aerolinea/src/main/java/org/example/aerolinea/package-info.java
@@ -2,7 +2,7 @@
 // Este archivo ha sido generado por la arquitectura JavaTM para la implantación de la referencia de enlace (JAXB) XML v2.2.7 
 // Visite <a href="http://java.sun.com/xml/jaxb">http://java.sun.com/xml/jaxb</a> 
 // Todas las modificaciones realizadas en este archivo se perderán si se vuelve a compilar el esquema de origen. 
-// Generado el: 2020.04.04 a las 05:01:17 PM CST 
+// Generado el: 2020.04.10 a las 01:59:06 AM CDT 
 //
 
 @javax.xml.bind.annotation.XmlSchema(namespace = "http://www.example.org/aerolinea", elementFormDefault = javax.xml.bind.annotation.XmlNsForm.QUALIFIED)
